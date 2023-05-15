d5e727ba51b1a9583ecba4d07dccbc200f92cbeb
Jose Almir <joseamfj@outlook.com>
2023-05-15 08:27:50 -0300

    Plot functions using sympy

--- Basics/plot_function.py
+++ Basics/plot_function.py
@@ -1,9 +1,17 @@
+from sympy import *
+
 # If f(x) = 2x - 4.
 def f(x):
     return 2*x - 4
 
 x_values = [2, 5, 7, 10]
 for x in x_values:
+    x = symbols('x')
     y = f(x)
-    print(y)
+    plot(y)
+
+
+
+
+
 
