98caa2b74cb0a32f3fcc1451d78db8533b649371
Jose Almir <joseamfj@outlook.com>
2023-05-16 12:27:58 -0300

    Simplify the calculate derivative code

--- Basics/limits/calculate_derivative.py
+++ Basics/limits/calculate_derivative.py
@@ -3,10 +3,9 @@ from sympy import *
 x, s = symbols('x s')
 f = x**2 + 3*x
 
-def slope_calc(x, s, f):
+def slope_formula(x, s, f):
     return (f.subs(x, x+s)-f)/((x+s) -x)
 
-slope_f = slope_calc(x, s, f)
-result = limit(slope_f, s, 0)
+result = limit(slope_formula(x, s, f), s, 0)
 
 print(result)
